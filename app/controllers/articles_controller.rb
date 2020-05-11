895c2d6bc524dc75bb43e31803f3e0fd2f2439ef
Meme <meme_es@hotmail.com>
2020-05-11 11:13:02 -0600

    Add the artifacts to edit an article
    This commit adds an 'update' action into the ArticlesControler and a 'edit.html.erb'
    view to edit articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,4 +26,15 @@ class ArticlesController < ApplicationController
 
     redirect_to articles_path
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
 end
