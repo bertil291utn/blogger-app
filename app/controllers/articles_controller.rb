2acd2263141090ecb4be843a7a1b6a5f9cfa53b6
Meme <meme_es@hotmail.com>
2020-05-11 11:25:48 -0600

    Add artifacts to show flash messages
    This commit adds a flash notice when there's an update, create or destroy
    action inside the application.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,12 +17,16 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+
+    flash.notice = "Article '#{@article.title}' Deleted!"
 
     redirect_to articles_path
   end
@@ -34,6 +38,8 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
 
     redirect_to article_path(@article)
   end
