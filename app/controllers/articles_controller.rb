636879810c3ce3d7d861a6e4dbf8d7c1922e5d62
Meme <meme_es@hotmail.com>
2020-05-11 10:59:00 -0600

    Add the artifacts of delete articles
    This commit adds a method 'destroy' in the ArticlesControler and the link into
    the 'show' view to delet an article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,4 +19,11 @@ class ArticlesController < ApplicationController
 
     redirect_to article_path(@article)
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
 end
