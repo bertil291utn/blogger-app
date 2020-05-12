ebb4316eb910c1a0716930ccd38b675db166c342
bertil291utn <bertilt4@gmail.com>
2020-05-12 16:37:22 -0500

    Migration database to schema

--- db/migrate/20200512213316_add_paperclip_fields_to_article.rb
+++ db/migrate/20200512213316_add_paperclip_fields_to_article.rb
@@ -1,4 +1,8 @@
 class AddPaperclipFieldsToArticle < ActiveRecord::Migration[5.2]
   def change
+    add_column :articles, :image_file_name, :string
+    add_column :articles, :image_content_type, :string
+    add_column :articles, :image_file_size, :integer
+    add_column :articles, :image_updated_at, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_155043) do
+ActiveRecord::Schema.define(version: 2020_05_12_213316) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
