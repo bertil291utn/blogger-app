2b6180436b8018290fdab9bda878424d7219a5a2
Meme <meme_es@hotmail.com>
2020-05-12 11:55:59 -0600

    Add methods and view to list and delete tags
    This commit adds a view to list all tags and a method to delete any tag.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,19 @@
 class TagsController < ApplicationController
+  def index
+    @tags = Tag.all
+  end
+
   def show
     @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+
+    @tag.destroy
+
+    flash.notice = "Tag '#{@tag.name}' deleted!"
+
+    redirect_to tags_path
   end
 end
