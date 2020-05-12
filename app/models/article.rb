3bc883bc94691bc584fc111636267fc6156078a3
bertil291utn <bertilt4@gmail.com>
2020-05-12 16:44:17 -0500

    Add image field to model and helper as param

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,16 +2,18 @@ class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ['image/jpg', 'image/jpeg', 'image/png']
 
   def tag_list
     self.tags.collect do |tag|
       tag.name
-    end.join(", ")
+    end.join(', ')
   end
 
   def tag_list=(tags_string)
-    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    tag_names = tags_string.split(',').collect { |s| s.strip.downcase }.uniq
     new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
-    self.tags = new_or_found_tags    
+    self.tags = new_or_found_tags
   end
 end
